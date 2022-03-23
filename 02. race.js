f99258d04827a835aeadeeb7d1be42f70fbbe1c4
ivodalemski <ivodalemski7@gmail.com>
2022-03-23 18:26:01 +0200

    first commit

---
+++ 02. race.js
@@ -0,0 +1,35 @@
+function race(input) {
+  let racers = {};
+  let names = input.shift().split(", ");
+  for (let line of input) {
+    if (line != "end of race") {
+      let racerName = line.match(/[A-Z]+/gi).join("");
+      let distanceInDigits = line.match(/\d/g);
+      let distance = 0;
+      for (let digit of distanceInDigits) {
+        distance += +digit;
+      }
+      if (names.includes(racerName)) {
+        if (racers.hasOwnProperty(racerName)) {
+          racers[racerName] += distance;
+        } else {
+          racers[racerName] = distance;
+        }
+      }
+    }
+  }
+  let sortedArr = Object.keys(racers).sort((a, b) => racers[b] - racers[a]);
+  console.log(`1st place: ${sortedArr[0]}`);
+  console.log(`2nd place: ${sortedArr[1]}`);
+  console.log(`3rd place: ${sortedArr[2]}`);
+}
+race([
+  "George, Peter, Bill, Tom",
+  "G4e@55or%6g6!68e!!@ ",
+  "R1@!3a$y4456@",
+  "B5@i@#123ll",
+  "G@e54o$r6ge#",
+  "7P%et^#e5346r",
+  "T$o553m&6",
+  "end of race",
+]);
