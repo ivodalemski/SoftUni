2b5b804c65def5e929b40d629433ebc1630cd1cd
ivodalemski <102073600+ivodalemski@users.noreply.github.com>
2022-05-17 23:02:18 +0300

    Delete javascript/Advanced/01. Syntax, Functions and Statements directory

--- javascript/Advanced/01. Syntax, Functions and Statements/02. String Length.js
+++
@@ -1,9 +0,0 @@
-function stringLength(first, second, third){
-    let firstLength = Number(first.length);
-    let secondLength = Number(second.length);
-    let thirdLength = Number(third.length);
-    let sum = firstLength + secondLength + thirdLength;
-    console.log(sum);
-    console.log(Math.trunc(sum / 3))
-}
-stringLength('chocolate', 'ice cream', 'cake')
--- javascript/Advanced/01. Syntax, Functions and Statements/08. Days in Month.js
+++
@@ -1,5 +0,0 @@
-function days(month, year) {
-  let newDate = new Date(year, month, 0).getDate();
-  console.log(newDate);
-}
-days(2, 2021);
